65ccaf1d632e03d6fa55f85098400f163c425435
Seif Mohamed <20p1710@eng.asu.edu.eg>
2024-10-24 17:49:51 +0300

    Add colors palette

---
+++ lib/colors.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+// Brand color: Coral
+const Map<int, Color> coral = {
+  100: Color(0xFFFFF4F0),
+  200: Color(0xFFFFE6D9),
+  300: Color(0xFFFFD1B8),
+  400: Color(0xFFFFA48A),
+  500: Color(0xFFFF705A),
+  600: Color(0xFFEB5753),
+  700: Color(0xFFD94542),
+  800: Color(0xFFC73132),
+  900: Color(0xFFAD251D),
+};
+
+// Secondary color: Teal
+const Map<int, Color> teal = {
+  100: Color(0xFFF0FDF4),
+  200: Color(0xFFDCFCE7),
+  300: Color(0xFFBBF7D0),
+  400: Color(0xFF86EFAC),
+  500: Color(0xFF4ADE80), // Secondary color (similar to Tailwind Teal 500)
+  600: Color(0xFF22C55E),
+  700: Color(0xFF16A34A),
+  800: Color(0xFF15803D),
+  900: Color(0xFF166534),
+};
+
+// Supporting color: Blue
+const Map<int, Color> blue = {
+  100: Color(0xFFDBEAFE),
+  200: Color(0xFFBFDBFE),
+  300: Color(0xFF93C5FD),
+  400: Color(0xFF60A5FA),
+  500: Color(0xFF3B82F6),
+  600: Color(0xFF2563EB),
+  700: Color(0xFF1D4ED8),
+  800: Color(0xFF1E40AF),
+  900: Color(0xFF1E3A8A),
+};
+
+// Supporting color: Red
+const Map<int, Color> red = {
+  100: Color(0xFFFEE2E2),
+  200: Color(0xFFFECACA),
+  300: Color(0xFFFCA5A5),
+  400: Color(0xFFF87171),
+  500: Color(0xFFEF4444),
+  600: Color(0xFFDC2626),
+  700: Color(0xFFB91C1C),
+  800: Color(0xFF991B1B),
+  900: Color(0xFF7F1D1D),
+};
+
+// Supporting color: Orange
+const Map<int, Color> orange = {
+  100: Color(0xFFFFF7ED),
+  200: Color(0xFFFFEDD5),
+  300: Color(0xFFFED7AA),
+  400: Color(0xFFFDBA74),
+  500: Color(0xFFFB923C),
+  600: Color(0xFFF97316),
+  700: Color(0xFFEA580C),
+  800: Color(0xFFC2410C),
+  900: Color(0xFF9A3412),
+};
+
+// Supporting color: Green
+const Map<int, Color> green = {
+  100: Color(0xFFD1FAE5),
+  200: Color(0xFFA7F3D0),
+  300: Color(0xFF6EE7B7),
+  400: Color(0xFF34D399),
+  500: Color(0xFF10B981),
+  600: Color(0xFF059669),
+  700: Color(0xFF047857),
+  800: Color(0xFF065F46),
+  900: Color(0xFF064E3B),
+};
+
+// Neutrals: Stone
+const Map<int, Color> stone = {
+  100: Color(0xFFFAF7F5),
+  200: Color(0xFFE7E5E4),
+  300: Color(0xFFD6D3D1),
+  400: Color(0xFFA8A29E),
+  500: Color(0xFF78716C),
+  600: Color(0xFF57534E),
+  700: Color(0xFF44403C),
+  800: Color(0xFF292524),
+  900: Color(0xFF1C1917),
+};
